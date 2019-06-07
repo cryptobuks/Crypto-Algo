4493a66e4b995ce5aa867bc1f2a0aa844652c159
bnonni <bryanwnonni@gmail.com>
2019-06-07 12:51:43 -0400

    Initiating files

---
+++ custom_modules/module.js
@@ -0,0 +1,33 @@
+/*jshint esversion: 6 */
+/**
+ * jQuery
+//  */
+// export function jQuery(){
+//   
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * RSI = 100−[ 100/(1+ (Average loss/Average gain))]
+ */
+
---
+++ server.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:63eb46acd07a8dd2ae597d2bb0fc95c12a056e9a63672349eaa7585177cd2a2d
+size 4178
